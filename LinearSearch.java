02ed285e5249351fa14ed0275f8f88ea5706ba8f
crabbyabby <abbylei76@gmail.com>
2026-03-25 15:05:01 -0400

    linear + selection done

--- LinearSearch.java
+++ LinearSearch.java
@@ -1,3 +1,5 @@
+import java.util.Collections;
+
 public class LinearSearch {
 
   public static boolean search(CardPile cards, Card target) {
@@ -5,6 +7,23 @@ public class LinearSearch {
   }
 
   public static boolean search(CardPile cards, Card target, SortRecorder record) {
+
+    if (record != null) {
+      record.add(cards);
+    }
+
+    for (Card card : cards) {
+      if (card.compareTo(target) == 0) {
+        return true;
+      }
+
+    if (record != null) {
+      record.next();
+      record.add(cards);
+    }
+
+    }
+
     // ***********************************************************
     // Search through the pile one card at a time.
     // Return true as soon as you find target.
@@ -16,5 +35,33 @@ public class LinearSearch {
     // ***********************************************************
 
     return false;
+  }
+
+  public static void main(String[] args) {
+    SortRecorder recorder = new SortRecorder();
+
+    // set up the deck of cards
+    Card.loadImages(recorder);
+    CardPile cards = new CardPile(Card.newDeck(true), 2, 2);
+
+    // for debugging purposes, uncomment this to
+    // work with a smaller number of cards:
+    // cards = cards.split(cards.get(39));
+
+    // mix up the cards
+    Collections.shuffle(cards);
+
+    // if you want to sort in array form, use:
+    Card[] card_arr = cards.toArray(new Card[0]);
+
+    // in your program, this would be a call to a real sorting algorithm
+    Card card = cards.get(5);
+    LinearSearch.search(cards, card, recorder);
+
+    // We can print out the (un)sorted result:
+    System.out.println(cards);
+
+    // make window appear showing the record
+    recorder.display("Card Sort Demo: LinearSearch");
   }
 }
--- SelectionSort.java
+++ SelectionSort.java
@@ -1,13 +1,36 @@
+import java.util.Collections;
+
 public class SelectionSort {
   
+  public static CardPile sort(CardPile unsorted) {
+    return(sort(unsorted, null));
+  }
+
   public static CardPile sort(CardPile unsorted, SortRecorder record) {
     
     // register the starting configuration with the recorder
-    record.add(unsorted);
-
+    if (record != null) {
+      record.add(unsorted);
+    }
     // Here is the result list you will be creating
     CardPile sorted = new CardPile();
-  
+    
+    while (unsorted.size() > 0) {
+      Card smallest = unsorted.peek();      for (Card card: unsorted) {
+        if (card.compareTo(smallest) < 0) {
+          smallest = card;
+        }
+      }
+      sorted.add(smallest);
+      unsorted.remove(smallest);
+      
+      if (record != null) {
+        record.next();
+        record.add(sorted);
+        record.add(unsorted);
+      }
+    }
+    
     // ***********************************************************
     // Here is where you'll do the "work" of SelectionSort:
     //   - Use sorted to store the "sorted portion"
@@ -20,5 +43,32 @@ public class SelectionSort {
 
     // return the sorted result here
     return sorted;
+  }
+
+    public static void main(String[] args) {
+      SortRecorder recorder = new SortRecorder();
+
+      // set up the deck of cards
+      Card.loadImages(recorder);
+      CardPile cards = new CardPile(Card.newDeck(true), 2, 2);
+
+      // for debugging purposes, uncomment this to
+      // work with a smaller number of cards:
+      // cards = cards.split(cards.get(39));
+
+      // mix up the cards
+      Collections.shuffle(cards);
+
+      // if you want to sort in array form, use:
+      Card[] card_arr = cards.toArray(new Card[0]);
+
+      // in your program, this would be a call to a real sorting algorithm
+      cards = SelectionSort.sort(cards, recorder);
+
+      // We can print out the (un)sorted result:
+      System.out.println(cards);
+
+      // make window appear showing the record
+      recorder.display("Card Sort Demo: SelectionSort");
   }
 }
